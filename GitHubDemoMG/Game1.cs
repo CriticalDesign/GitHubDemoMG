8384b1fff8e043491e4f348a9d37f9c2102b07e3
Aaron <aaron.langille@gmail.com>
2026-01-21 13:04:38 -0500

    Added speed variable.

--- GitHubDemoMG/Game1.cs
+++ GitHubDemoMG/Game1.cs
@@ -8,7 +8,7 @@ namespace GitHubDemoMG
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        private float speed;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
